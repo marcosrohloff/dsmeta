f76cddedb586b6db4fe065f76b0ce5854e146476
Marcos Fernando <marcos.fersh@gmail.com>
2022-07-13 01:57:13 -0300

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,5 @@
-import icon from '../../assets/img/notification-icon.svg'
-import './styles.css'
+import icon from '../../assets/img/notification-icon.svg';
+import './styles.css';
 
 function NotifcationButton() {
     return(
@@ -9,4 +9,4 @@ function NotifcationButton() {
     )
   }
   
-  export default NotifcationButton
+  export default NotifcationButton;
