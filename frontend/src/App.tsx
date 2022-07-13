68065f09be0786500260733e817dc73b8b9c1e85
Marcos Fernando <marcos.fersh@gmail.com>
2022-07-13 01:35:51 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import Header from "./components/Header"
 import NotifcationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return(
     <>
-      <h1>Ola teste</h1>
-      <NotifcationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
